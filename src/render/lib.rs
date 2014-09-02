d0bd7ba0b1c45659c2de895030805f5c57b6658b
kvark <kvark@seemeroll.in>
2014-09-01 23:08:27 -0400

    Implemented draw call sorting and rendering

--- src/render/lib.rs
+++ src/render/lib.rs
@@ -6,30 +6,33 @@ extern crate cgmath;
 extern crate ecs;
 extern crate gfx;
 
+use std::{iter, slice};
+
 // normalized depth
 pub type Depth = u16;
 
 pub type Scalar = f32;
 
 pub type Transform = cgmath::Decomposed<
-	Scalar,
-	cgmath::Quaternion<Scalar>,
-	cgmath::Point3<Scalar>
+    Scalar,
+    cgmath::Quaternion<Scalar>,
+    cgmath::Point3<Scalar>
 >;
 
 struct Camera {
-	transform: Transform,
-	frustum: cgmath::Frustum<Scalar>,
+    transform: Transform,
+    frustum: cgmath::Frustum<Scalar>,
 }
 
 pub struct Object<L, T> {
-	batch: gfx::batch::RefBatch<L, T>,
+    batch: gfx::batch::RefBatch<L, T>,
+    data: T,
     bounding_sphere: cgmath::Sphere<Scalar>,
-	depth: Depth,
+    depth: Depth,
 }
 
 pub fn order_opaque<L, T>(a: &Object<L, T>, b: &Object<L, T>) -> Ordering {
-    Equal
+    (&a.batch, a.depth).cmp(&(&b.batch, b.depth))
 }
 
 type Index = uint;
@@ -37,12 +40,27 @@ type Index = uint;
 pub struct Queue<L, T> {
     pub objects: Vec<Object<L, T>>,
     indices: Vec<Index>,
-	context: gfx::batch::Context,
+    context: gfx::batch::Context,
 }
 
-impl<L, T> Queue<L, T> {
-    fn update(&mut self, order: |&Object<L, T>, &Object<L, T>| -> Ordering) {
-        // synchronize indices to have the same length
+struct ObjectIter<'a, L: 'a, T: 'a> {
+    objects: &'a [Object<L, T>],
+    id_iter: slice::Items<'a, Index>,
+}
+
+impl<'a, L, T> Iterator<&'a Object<L, T>> for ObjectIter<'a, L, T> {
+    fn next(&mut self) -> Option<&'a Object<L, T>> {
+        self.id_iter.next().map(|&i| &self.objects[i])
+    }
+}
+
+impl<L, T: gfx::shade::ShaderParam<L>> Queue<L, T> {
+    fn is_updated(&self) -> bool {
+        self.objects.len() == self.indices.len()
+    }
+
+    /// Synchronize indices to have the same length as objects
+    pub fn update(&mut self) {
         let ni = self.indices.len();
         if self.objects.len() > ni {
             self.indices.grow_fn(self.objects.len() - ni, |i| (ni + i) as Index);
@@ -50,25 +68,47 @@ impl<L, T> Queue<L, T> {
         if self.objects.len() < ni {
             self.indices.retain(|&i| (i as uint) < ni);
         }
-        debug_assert_eq!(self.objects.len(), self.indices.len());
-        // sort
+        debug_assert!(self.is_updated());
+    }
+
+    /// Sort objects with the given order
+    pub fn sort(&mut self, order: |&Object<L, T>, &Object<L, T>| -> Ordering) {
+        debug_assert!(self.is_updated());
         let objects = self.objects.as_slice();
         self.indices.sort_by(|&ia, &ib|
             (order)(&objects[ia], &objects[ib])
         );
-        // done
+    }
+
+    /// Iterate objects in the sorted order
+    pub fn objects<'a>(&'a self) -> ObjectIter<'a, L, T>  {
+        debug_assert!(self.is_updated());
+        ObjectIter {
+            objects: self.objects.as_slice(),
+            id_iter: self.indices.iter(),
+        }
+    }
+
+    // render everything to the given frame
+    pub fn render<C: gfx::CommandBuffer>(&self, renderer: &mut gfx::Renderer<C>,
+                  frame: &gfx::Frame) {
+        debug_assert!(self.is_updated());
+        for &i in self.indices.iter() {
+            let ob = &self.objects[i];
+            renderer.draw((&ob.batch, &ob.data, &self.context), frame);
+        }
     }
 }
 
 pub struct View<L, T> {
     pub frame: gfx::Frame,
     pub camera: Camera,
-	pub queue: Queue<L, T>,
+    pub queue: Queue<L, T>,
 }
 
 impl<L, T> View<L, T> {
-	fn add(&mut self, batch: gfx::batch::RefBatch<L, T>, bound: cgmath::Sphere<Scalar>) {
-		let _depth = 0u;
-		//self.queue.objects.push(Object {})
-	}
+    fn add(&mut self, batch: gfx::batch::RefBatch<L, T>, bound: cgmath::Sphere<Scalar>) {
+        let _depth = 0u;
+        //self.queue.objects.push(Object {})
+    }
 }
